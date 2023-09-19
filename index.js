997ea9d8a54b12ea00a97084dd1388439d1c7882
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-09-19 23:09:34 +0530

    POSTing data to server

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ const friends = [
 const server = http.createServer();
 server.on("request", (req, res) => {
   const items = req.url.split("/");
-  if (items[1] === "friends") {
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log(`Request: ${friend}`);
+      friends.push(JSON.parse(friend));
+    });
+  } else if (req.method === "GET" && items[1] === "friends") {
     res.writeHead(200, {
       "Content-Type": "application/json",
     });
@@ -30,7 +36,7 @@ server.on("request", (req, res) => {
     } else {
       res.end(JSON.stringify(friends));
     }
-  } else if (items[1] === "messages") {
+  } else if (req.method === "GET" && items[1] === "messages") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
